a69fb078d4a87923e70982f776d6d0b3fa781801
Pwn9 <admin@pwn9.com>
2012-12-24 13:03:01 -0500

    Solid revisions and potential release with working config.
    Config works.. entity check appears to work. Lets roll.

--- src/com/pwn9/PwnChickenLay/PwnChickenLay.java
+++ src/com/pwn9/PwnChickenLay/PwnChickenLay.java
@@ -1,40 +1,55 @@
 package com.pwn9.PwnChickenLay;
 
+import java.util.List;
+import java.util.Random;
 import java.util.logging.Logger;
 
-import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginManager;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.ItemSpawnEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PwnChickenLay extends JavaPlugin {
 	
-	private PluginManager pm;
-	private Logger log = Logger.getLogger("Minecraft");
-	private PwnChickenLayItemSpawnListener itemSpawnListener = new PwnChickenLayItemSpawnListener();
-	//private PwnChickenLayItemSpawnListener itemSpawnListener;
-	
-	@Override
+	public final Logger logger = Logger.getLogger("Minecraft.PwnChickenLay");   	
+
 	public void onEnable() {
-		this.saveDefaultConfig();
-		this.pm = this.getServer().getPluginManager();
-		this.regListeners();
-		this.logMsg("Loaded.");
+    	this.saveDefaultConfig();
+    	new PwnChickenLayItemSpawnListener(this);
 	}
 	
-	private void regListeners() {
-		pm.registerEvents(this.itemSpawnListener, this);
-	}
-	
-	private void logMsg(String msg) {
-		PluginDescriptionFile pdf = this.getDescription();
-		this.log.info(pdf.getName() + " " + pdf.getVersion() + ": " + msg);
-	}
-	
-	@Override
 	public void onDisable() {
-		this.logMsg("Unloaded.");	
+
 	}	
 	
-	//public String layChance = PwnChickenLay.this.getConfig().getString("laychance");
+	public void spawnCheck(ItemSpawnEvent event) {
+		if(event.isCancelled()) {
+			return;
+		}
 		
+		Item test = (Item) event.getEntity();
+		
+		ItemStack is = test.getItemStack();
+		if(is.getType() != Material.EGG) {
+			return;
+		}
+		
+		List<Entity> nearby = test.getNearbyEntities(0.01, 0.3, 0.01);
+		
+		for(int i = 0; i < nearby.size(); i++) {
+			if (nearby.get(i) instanceof Player) {
+			return;
+			}
+		}
+		
+		Random random = new Random();
+		int randomInt = random.nextInt(100);
+    	String layChance = getConfig().getString("layChance");
+		if (randomInt > Integer.parseInt(layChance)) {	
+			event.setCancelled(true);
+		}
+	}	
 }
--- src/com/pwn9/PwnChickenLay/PwnChickenLayItemSpawnListener.java
+++ src/com/pwn9/PwnChickenLay/PwnChickenLayItemSpawnListener.java
@@ -1,51 +1,19 @@
 package com.pwn9.PwnChickenLay;
 
-import java.util.List;
-import java.util.Random;
-
-import org.bukkit.Material;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ItemSpawnEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class PwnChickenLayItemSpawnListener implements Listener {
+    private final PwnChickenLay plugin;
+	public PwnChickenLayItemSpawnListener(PwnChickenLay plugin) {
+	    plugin.getServer().getPluginManager().registerEvents(this, plugin);    
+	    this.plugin = plugin;
+	}
 
-	//private PwnChickenLay plugin;
-	
-	//public PwnChickenLayItemSpawnListener(PwnChickenLay instance) {
-	//	plugin = instance;
-	//}
-	
-	@EventHandler
+	@EventHandler(priority = EventPriority.LOWEST)
 	public void onSpawn(ItemSpawnEvent event) {
-		if(event.isCancelled()) {
-			return;
-		}
-		
-		Item test = (Item) event.getEntity();
-		
-		ItemStack is = test.getItemStack();
-		if(is.getType() != Material.EGG) {
-			return;
-		}
-		
-		List<Entity> nearby = test.getNearbyEntities(0.01, 0.3, 0.01);
-		
-		for(int i = 0; i < nearby.size(); i++) {
-			// Player: org.bukkit.craftbukkit.entity.CraftPlayer
-			if(nearby.get(i).getClass().getName() == "org.bukkit.craftbukkit.entity.CraftPlayer") {
-				return;
-			}
-		}
-		
-		Random random = new Random();
-		int randomInt = random.nextInt(100);
-		//if (randomInt > Integer.parseInt(plugin.layChance)) {
-		if (randomInt > 10) {		
-			event.setCancelled(true);
-		}
-	}	
+		plugin.spawnCheck(event);
+	}  
 }
