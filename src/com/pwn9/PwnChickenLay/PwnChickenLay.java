3a11dea4b00890bc0a25ecbb4167b032d466d44d
tremor <admin@pwn9.com>
2013-09-06 21:29:08 -0400

    Configurable Egg Drops

--- src/com/pwn9/PwnChickenLay/PwnChickenLay.java
+++ src/com/pwn9/PwnChickenLay/PwnChickenLay.java
@@ -12,44 +12,70 @@ import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class PwnChickenLay extends JavaPlugin {
+public class PwnChickenLay extends JavaPlugin 
+{
 	
 	public final Logger logger = Logger.getLogger("Minecraft.PwnChickenLay");   	
-
-	public void onEnable() {
+	public static List<String> enabledWorlds;
+	public static List<String> replaceWith;
+	public int layChance;
+	static Random randomNumberGenerator = new Random();
+	
+	public void onEnable() 
+	{
     	this.saveDefaultConfig();
     	new PwnChickenLayItemSpawnListener(this);
+    	PwnChickenLay.enabledWorlds = getConfig().getStringList("enabled_worlds");
+    	this.layChance = getConfig().getInt("layChance");
+    	PwnChickenLay.replaceWith = getConfig().getStringList("replaceWith");
 	}
 	
-	public void onDisable() {
+	public void onDisable() 
+	{
 
 	}	
 	
-	public void spawnCheck(ItemSpawnEvent event) {
-		if(event.isCancelled()) {
+	public void spawnCheck(ItemSpawnEvent event) 
+	{
+		if(event.isCancelled()) 
+		{
 			return;
 		}
 		
 		Item test = (Item) event.getEntity();
 		
 		ItemStack is = test.getItemStack();
-		if(is.getType() != Material.EGG) {
+		
+		if(is.getType() != Material.EGG) 
+		{
 			return;
 		}
 		
 		List<Entity> nearby = test.getNearbyEntities(0.01, 0.3, 0.01);
 		
-		for(int i = 0; i < nearby.size(); i++) {
-			if (nearby.get(i) instanceof Player) {
-			return;
+		for(int i = 0; i < nearby.size(); i++) 
+		{
+			if (nearby.get(i) instanceof Player)
+			{
+				return;
 			}
 		}
-		
-		Random random = new Random();
-		int randomInt = random.nextInt(100);
-    	String layChance = getConfig().getString("layChance");
-		if (randomInt > Integer.parseInt(layChance)) {	
-			event.setCancelled(true);
+		  	
+    	if (PwnChickenLay.random(layChance)) 
+		{	
+    		String randomReplacement = replaceWith.get(randomNumberGenerator.nextInt(replaceWith.size()));     	
+			//event.setCancelled(true);
+			is.setType(Material.getMaterial(randomReplacement));
 		}
 	}	
+
+	static boolean random(int percentChance) {
+		return randomNumberGenerator.nextInt(100) > percentChance;
+	}
+	
+	public static boolean isEnabledIn(String world) 
+	{
+		return enabledWorlds.contains(world);
+	}	
+	
 }
--- src/com/pwn9/PwnChickenLay/PwnChickenLayItemSpawnListener.java
+++ src/com/pwn9/PwnChickenLay/PwnChickenLayItemSpawnListener.java
@@ -1,19 +1,28 @@
 package com.pwn9.PwnChickenLay;
 
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ItemSpawnEvent;
 
-public class PwnChickenLayItemSpawnListener implements Listener {
+public class PwnChickenLayItemSpawnListener implements Listener 
+{
     private final PwnChickenLay plugin;
-	public PwnChickenLayItemSpawnListener(PwnChickenLay plugin) {
+    
+	public PwnChickenLayItemSpawnListener(PwnChickenLay plugin) 
+	{
 	    plugin.getServer().getPluginManager().registerEvents(this, plugin);    
 	    this.plugin = plugin;
 	}
 
 	@EventHandler(priority = EventPriority.LOWEST)
-	public void onSpawn(ItemSpawnEvent event) {
-		plugin.spawnCheck(event);
+	public void onSpawn(ItemSpawnEvent event) 
+	{
+		World world = event.getLocation().getWorld();
+		if (PwnChickenLay.isEnabledIn(world.getName())) 
+		{
+			plugin.spawnCheck(event);
+		}
 	}  
 }
