4760cbd8570897e5783d3296c827f69007e2edf1
tremor <admin@pwn9.com>
2014-01-18 12:30:17 -0500

    Fixed replacing egg with air issues in bukkit 1.7

--- src/com/pwn9/PwnChickenLay/PwnChickenLay.java
+++ src/com/pwn9/PwnChickenLay/PwnChickenLay.java
@@ -11,12 +11,6 @@ import java.util.List;
 import java.util.Random;
 import java.util.logging.Logger;
 
-import org.bukkit.Material;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
-import org.bukkit.event.entity.ItemSpawnEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -75,67 +69,6 @@ public class PwnChickenLay extends JavaPlugin
 		{		
 			PwnChickenLay.logToFile(PwnChickenLay.pdfFile.getName() + " version " + PwnChickenLay.pdfFile.getVersion() + " [disabled]");
 		}	
-	}	
-	
-	// Main function to receive event data and determine if it is a chicken laying the egg, then do stuff...
-	public void spawnCheck(ItemSpawnEvent event, String world) 
-	{
-		// if event is already cancelled by another just pass it on
-		if(event.isCancelled()) 
-		{
-			return;
-		}
-		
-		Item test = (Item) event.getEntity();
-		
-		ItemStack is = test.getItemStack();
-		
-		// if item is anything other than an egg, return
-		if(is.getType() != Material.EGG) 
-		{
-			return;
-		}
-		
-		List<Entity> nearby = test.getNearbyEntities(0.01, 0.3, 0.01);
-		
-		// check if a player entity is at the same location as the ItemSpawnEvent - if so, probably not a chicken lay and return
-		for(int i = 0; i < nearby.size(); i++) 
-		{
-			if (nearby.get(i) instanceof Player)
-			{
-				return;
-			}
-		}
-		
-		// check per world settings
-		if (getConfig().getBoolean("perWorld."+world+".enabled")) 
-		{
-			if (PwnChickenLay.random(getConfig().getInt("perWorld."+world+".layChance")))
-			{	
-				List<String> repWith = getConfig().getStringList("perWorld."+world+".replaceWith");		
-	    		String randomReplacement = repWith.get(randomNumberGenerator.nextInt(repWith.size()));     	
-				//event.setCancelled(true);
-				is.setType(Material.getMaterial(randomReplacement));
-				
-				// log if debug_log is enabled
-				if (PwnChickenLay.logEnabled)
-				{		
-					PwnChickenLay.logToFile("Chicken laid: " + randomReplacement + " in world: " + world);
-				}
-			}			
-		}
-		else if (PwnChickenLay.random(layChance)) 
-		{	
-    		String randomReplacement = replaceWith.get(randomNumberGenerator.nextInt(replaceWith.size()));     	
-			//event.setCancelled(true);
-			is.setType(Material.getMaterial(randomReplacement));
-			
-			// log if debug_log is enabled
-			if (PwnChickenLay.logEnabled)
-			{		
-				PwnChickenLay.logToFile("Chicken laid: " + randomReplacement + " in world: " + world);
-			}			
-		}
 	}	
 	
 	// Generate a random number and return bool
--- src/com/pwn9/PwnChickenLay/PwnChickenLayItemSpawnListener.java
+++ src/com/pwn9/PwnChickenLay/PwnChickenLayItemSpawnListener.java
@@ -1,10 +1,18 @@
 package com.pwn9.PwnChickenLay;
 
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ItemSpawnEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class PwnChickenLayItemSpawnListener implements Listener 
 {
@@ -17,13 +25,87 @@ public class PwnChickenLayItemSpawnListener implements Listener
 	}
 
 	// List for the ItemSpawnEvent and then kick it to the spawnCheck function 
-	@EventHandler(priority = EventPriority.LOWEST)
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onSpawn(ItemSpawnEvent event) 
 	{
-		World world = event.getLocation().getWorld();
-		if (PwnChickenLay.isEnabledIn(world.getName())) 
+		World eworld = event.getLocation().getWorld();
+		Location eLoc = event.getLocation();
+		// If plugin is not enabled in this world, return
+		if (!PwnChickenLay.isEnabledIn(eworld.getName())) return; 
+
+		Item test = (Item) event.getEntity();
+		ItemStack is = test.getItemStack();
+		
+		// if item is anything other than an egg, return
+		if(is.getType() != Material.EGG) return;
+
+		List<Entity> nearby = test.getNearbyEntities(0.01, 0.3, 0.01);
+		
+		// check if a player entity is at the same location as the ItemSpawnEvent - if so, probably not a chicken lay and return
+		for(int i = 0; i < nearby.size(); i++) 
 		{
-			plugin.spawnCheck(event, world.getName());
+			if (nearby.get(i) instanceof Player) return;
 		}
+		
+		String world = eworld.getName();
+		
+		// check per world settings
+		if (plugin.getConfig().getBoolean("perWorld."+world+".enabled")) 
+		{
+			if (PwnChickenLay.random(plugin.getConfig().getInt("perWorld."+world+".layChance")))
+			{	
+				List<String> repWith = plugin.getConfig().getStringList("perWorld."+world+".replaceWith");		
+				String randomReplacement = repWith.get(PwnChickenLay.randomNumberGenerator.nextInt(repWith.size()));     	
+				
+	    		//is.setType(Material.getMaterial(randomReplacement));
+
+				event.getEntity().remove();
+				event.setCancelled(true);
+				
+				if (Material.getMaterial(randomReplacement) != Material.AIR) 
+				{
+					ItemStack drop = new ItemStack(Material.getMaterial(randomReplacement), 1);
+					eworld.dropItem(eLoc, drop);
+				}
+				
+	    		// log if debug_log is enabled
+				if (PwnChickenLay.logEnabled)
+				{		
+					PwnChickenLay.logToFile("Chicken laid: " + randomReplacement + " in world: " + world);
+				}
+				
+			}
+		}
+		else if (PwnChickenLay.random(PwnChickenLay.layChance)) 
+		{	
+    		String randomReplacement = PwnChickenLay.replaceWith.get(PwnChickenLay.randomNumberGenerator.nextInt(PwnChickenLay.replaceWith.size()));
+    		
+    		//is.setType(Material.getMaterial(randomReplacement));
+			
+			event.getEntity().remove();
+			event.setCancelled(true);
+			
+			if (Material.getMaterial(randomReplacement) != Material.AIR) 
+			{
+				ItemStack drop = new ItemStack(Material.getMaterial(randomReplacement), 1);
+				eworld.dropItem(eLoc, drop);
+			}
+			
+    		// log if debug_log is enabled
+			if (PwnChickenLay.logEnabled)
+			{		
+				PwnChickenLay.logToFile("Chicken laid: " + randomReplacement + " in world: " + world);
+			}	
+			
+		}
+		else 
+		{
+			// log if debug_log is enabled
+			if (PwnChickenLay.logEnabled)
+			{		
+				PwnChickenLay.logToFile("Chicken laid: Default egg, in world: " + world);
+			}					
+		}
+		
 	}  
 }
